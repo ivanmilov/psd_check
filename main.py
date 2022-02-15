52d610e724b916d237309354f4836a6408eb197b
Ivan <522518+ivanmilov@users.noreply.github.com>
2022-02-15 18:39:05 +0100

    output

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ transtab = str.maketrans('', '', string.ascii_letters + string.digits + '_')
 
 def check(name):
     res = name.translate(transtab)
-    if len(res) > 0:
-        print('-> ', name, ': ', res)
+    # if len(res) > 0:
+    #     print('->', name, ': ', res)
+    return len(res) > 0, res
 
 
 def check_layer(layer):
+    err = False
+    layers = []
     if layer.is_group():
-        print('group', layer.name)
+        # print('group', layer.name)
         for l in layer:
-            check_layer(l)
-    check(layer.name)
-    print('\tlayer: ', layer.name)
+            e, l = check_layer(l)
+            err = err or l
+            layers.extend(l)
+    check_err, res = check(layer.name)
+    if check_err:
+        err = True
+        layers.append([layer.name, res])
+        # print('\tNOK layer: ', layer.name)
+
+    return err, layers
 
 
 if __name__ == '__main__':
@@ -26,5 +36,14 @@ if __name__ == '__main__':
         psd = PSDImage.open(file)
         base = os.path.splitext(file)[0]
 
+        errs = []
+
         for layer in psd:
-            check_layer(layer)
+            e, l = check_layer(layer)
+            errs.extend(l)
+        if len(errs) > 0:
+            print("file:", file, ":")
+            for t in errs:
+                print("\tlayer[{}] -> [{}]".format(t[0], t[1]))
+        else:
+            print("file:", file, " OK")
