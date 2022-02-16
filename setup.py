166e5600216ae5978df09dc5593dfefb8177c0eb
Ivan <522518+ivanmilov@users.noreply.github.com>
2022-02-16 19:03:28 +0100

    installer

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='psd_check',
+      version='1.0',
+      description='Check and rename layout names',
+      author='Ivan Milov',
+      url='https://github.com/ivanmilov/psd_check',
+      scripts=[
+          'psd_check'
+      ]
+      )
